de5f4e63ef8a9ded0b73fb2701b61e8bc483410f
holly <holly.gummersonn@gmail.com>
2025-01-18 12:37:51 -0800

    hoverplay functionality for demos

--- javascript/index_script.js
+++ javascript/index_script.js
@@ -1,6 +1,6 @@
 
 
-// Further contact button
+// FURTHER CONTACT BUTTON
 document.getElementById("contact").innerHTML = "Would you like to contact me further?";
 
 function updateContact(message) {
@@ -11,5 +11,22 @@ function hide(button) {
     var div = document.getElementById(button);
     div.style.display = 'none';
 }
+
+// HOVER PLAY FUNCTIONALITY
+function hoverPlay(videoId) {
+    const video = document.getElementById(videoId);
+    video.addEventListener("mouseover", function() {
+        this.play();
+    });
+
+    video.addEventListener("mouseleave", function() {
+        this.pause();
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    hoverPlay("webvid");
+    hoverPlay("appvid");
+});
 
 // TODO, fade as you scroll, hover stuff for projects, scroll for resume, other
